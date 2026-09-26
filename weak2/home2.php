661f28bdcd73060ab41c849a82d08c81cf2005a1
Sumaya Elmi <suuelmi057@gmail.com>
2026-09-26 22:29:28 +0300

    Update weak2 PHP practice

--- weak2/home2.php
+++ weak2/home2.php
@@ -16,6 +16,7 @@ echo age . "<br>";
 
 
 // 2. If-Else: checks whether the age is 18 or older and displays "adult"; otherwise, it displays "child".
+
 $age = 20;
 
 if ($age >= 18) {
@@ -26,6 +27,7 @@ if ($age >= 18) {
 
 
 // 3. Switch: checks the student's marks against different conditions and displays the appropriate grade.
+
 $marks = 100;
 
 switch (true) {
@@ -52,6 +54,7 @@ switch (true) {
 
 
 // 4. While Loop: repeats the code as long as the condition is true, printing numbers from 1 to 5.
+
 $age = 1;
 
 while ($age <= 5) {
@@ -62,6 +65,7 @@ while ($age <= 5) {
 
 
 // 5. Do-While Loop: executes the code at least once and continues printing even numbers from 2 to 10.
+
 $number = 2;
 
 do {
@@ -79,6 +83,7 @@ echo "<br>";
 
 
 // 7. For Loop: uses a counter to print numbers sequentially from 1 to 15.
+
 for ($count = 1; $count <= 15; $count++) {
     echo "$count" . "<br>";
 }
@@ -87,14 +92,74 @@ echo "<br>";
 
 
 // 8. For Loop: calculates and displays the square of each number from 1 to 10.
+
 for ($i = 1; $i <= 10; $i++) {
     echo "The square of $i is " . $i * $i . "<br>";
+
+    //// maanta
+    // 1. Creates an empty indexed array.
+$names = array();
+
+
+// 2. Adds values to the array using numeric indexes.
+$names[0] = "sumaya im programmer";
+$names[1] = 123;
+$names[] = 12.34;
+
+
+// 3. Displays each value from the array.
+echo $names[0] . "<br>";
+echo $names[1] . "<br>";
+echo $names[2] . "<br>";
+
+
+// 4. Displays all array values and their data types.
+echo "<br>";
+echo "<pre>";
+var_dump($names);
+echo "</pre>";
+
+
+// 5. Creates an indexed array with personal information.
+$info = array(
+    "101",
+    "Upah omar alasow",
+    20,
+    "Karan District",
+    "single"
+);
+
+
+// 6. Uses a for loop to display all array values.
+echo "Array values using for loop: <br>";
+
+for ($i = 0; $i < count($info); $i++) {
+    echo $info[$i] . "<br>";
+}
+
+
+// 7. Creates an associative array using named keys.
+$info = array(
+    "id" => "101",
+    "name" => "sumaya elmi",
+    "age" => 20,
+    "address" => "yaqshiid District",
+    "status" => "single",
+    "weight" => 160.5
+);
+
+
+// 8. Displays the associative array information.
+echo "<pre>";
+echo "Information about the person: <br>";
+print_r($info);
+var_dump($info);
+echo "</pre>";
+
 }
 
 ?>
 
-
-                   
-  
+ 
 </body>
 </html>
